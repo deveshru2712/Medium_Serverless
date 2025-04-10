4faa1120d2b5fd2de84a3d4e6b933b85108d55b5
Devesh Chandra <deveshru2712@gmail.com>
2025-04-10 13:28:40 +0530

    complete navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <nav className="w-full h-16 py-4 border-b shadow-md">
       <div className="h-full px-4 flex justify-between items-center">
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <Link to={"/"}>
             <h1 className="text-2xl font-semibold font-segoeu-heavy">
               Storyscape
             </h1>
           </Link>
-          <div className="bg-[#F9F9F9] border border-slate-200/50  gap-2 rounded-3xl flex p-1">
+          <div className="hidden md:flex bg-[#F9F9F9] gap-2 rounded-3xl p-1">
             <button type="submit" className="cursor-pointer">
               <Search />
             </button>
@@ -23,9 +23,14 @@ const Navbar = () => {
               />
             </form>
           </div>
+
+          <button className="cursor-pointer block md:hidden">
+            <Search />
+          </button>
         </div>
+
         <div className="flex justify-between items-center gap-8">
-          <button className="flex cursor-pointer">
+          <button className="hidden md:flex cursor-pointer">
             <NotebookPen />
             Write
           </button>
