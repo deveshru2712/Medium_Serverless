bdb0130e4094ce6f410dc498ac5a2c25b6ea57a2
Devesh Chandra <deveshru2712@gmail.com>
2025-04-19 00:01:39 +0530

    updated the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,40 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Bell, NotebookPen, Search, User } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const dropDownRef = useRef<HTMLDivElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        dropDownRef.current &&
+        !dropDownRef.current.contains(e.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(e.target as Node)
+      ) {
+        setActive(false);
+      }
+    };
+
+    if (active) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [active]);
+
+  const handleClick = () => {
+    setActive((prev) => !prev);
+  };
+
   const navigate = useNavigate();
+
   return (
     <nav className="w-full h-16 py-4 border-b shadow-md">
       <div className="h-full px-4 flex justify-between items-center">
@@ -44,10 +76,29 @@ const Navbar = () => {
           </button>
 
           <button
-            className="cursor-pointer"
-            onClick={() => navigate("/update")}
+            ref={buttonRef}
+            className="cursor-pointer relative"
+            onClick={handleClick}
           >
             <User />
+            <div
+              ref={dropDownRef}
+              className={`w-fit bg-white ${
+                active ? "block" : "hidden"
+              } absolute -left-1/2 top-1/6 -translate-x-3/4 translate-y-1/2 px-3 py-2 border rounded-md`}
+            >
+              <ul className="flex flex-col justify-center items-start text-lg font-semibold whitespace-nowrap">
+                <li className="hover:underline">
+                  <Link to={"/update"}>My profile</Link>
+                </li>
+                <li className="hover:underline">
+                  <Link to={"/myblog"}>My Blog</Link>
+                </li>
+                <li className="hover:underline">
+                  <Link to={"/logout"}>Logout</Link>
+                </li>
+              </ul>
+            </div>
           </button>
         </div>
       </div>
